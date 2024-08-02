011d4eaafb34a346f031581b95ece4afb7bb4737
Shamim2601 <shamim2601@gmail.com>
2024-08-02 22:50:36 +0600

    ssl

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: "postgres://default:YJu1khP0KqmS@ep-old-dust-a4tavg32-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+  connectionString: "postgres://default:YJu1khP0KqmS@ep-old-dust-a4tavg32-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
 })
