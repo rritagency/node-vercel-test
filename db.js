d9f5346e27d1506f51206c1c1d88a58545522220
Shamim2601 <shamim2601@gmail.com>
2024-08-02 23:03:57 +0600

    vercel/postgres

--- db.js
+++ db.js
@@ -1,5 +1,18 @@
-const { Pool } = require('pg')
+const { createPool } = require('@vercel/postgres');
 
-const pool = new Pool({
-  connectionString: "postgres://default:YJu1khP0KqmS@ep-old-dust-a4tavg32-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
-})
+const pool = createPool({
+  connectionString: 'postgres://default:YJu1khP0KqmS@ep-old-dust-a4tavg32-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require',
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
