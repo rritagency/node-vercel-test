17ee7518e079001811bfcd199f49d58714c8660a
Shamim2601 <shamim2601@gmail.com>
2024-08-02 22:39:37 +0600

    m

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+// Root route
 router.get("/", async (req, res) => {
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
   });
+});
+
+// /todos route
+router.get("/todos", async (req, res) => {
+  return res.status(200).json([
+    { id: 1, task: "Do the laundry", completed: false },
+    { id: 2, task: "Clean the house", completed: true },
+    { id: 3, task: "Buy groceries", completed: false },
+  ]);
 });
 
 module.exports = router;
